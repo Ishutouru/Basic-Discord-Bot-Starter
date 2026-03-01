0638cb81f02876ba3d274d58b5286e7da4e7381f
Ishu <oonay668@gmail.com>
2026-03-01 08:33:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const{token} = require('./config.json');
 const{Client, GatewayIntentBits} = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] });
+client.on('clientReady', () => {
+	console.log(`Bot is online as ${client.user.tag}`);
+});
 
 client.login(token);
